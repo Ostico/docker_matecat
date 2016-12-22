d90169ae03e2e928edef63051a25e7c00f71c63c
domenico <domenico@translated.net>
2016-12-22 18:08:18 +0100

    Replication struct and proxySQL

--- MateCat-Trusty/MateCatFilters/startFilter.sh
+++ MateCat-Trusty/MateCatFilters/startFilter.sh
@@ -9,16 +9,17 @@ if [ ! -d "/var/www/MateCat-Filters/" ]; then
 
     echo "build matecat filters layer"
     git clone https://github.com/matecat/MateCat-Filters.git
-    cd /var/www/okapi/MateCat-Filters
+    cd /var/www/okapi/MateCat-Filters/filters
     mvn clean package -DskipTests=true
 
     echo "Setting configurations"
-    cd /var/www/okapi/MateCat-Filters/target
+    cd /var/www/okapi/MateCat-Filters/filters/target
     cp ../src/main/resources/config.sample.properties config.properties
     mkdir -p /var/www/MateCat-Filters/
-    cp -a /var/www/okapi/MateCat-Filters/target/* /var/www/MateCat-Filters/
+    cp -a /var/www/okapi/MateCat-Filters/filters/target/* /var/www/MateCat-Filters/
 
     echo "Purge Maven"
+    cd ..
     mvn dependency:purge-local-repository -DactTransitively=false -DreResolve=false
     rm -rf /var/www/okapi
 fi
@@ -26,7 +27,8 @@ fi
 echo "Starting Filter\n"
 cd /var/www/MateCat-Filters/
 
-java -cp ".:filters-1.1.3.jar" com.matecat.converter.Main
+FILTER_BUILD=$(ls | grep -E "^filters-[0-9\.]+.jar$")
+java -cp ".:${FILTER_BUILD}" com.matecat.converter.Main
 
 RET=1
 while [[ RET -ne 0 ]]; do
