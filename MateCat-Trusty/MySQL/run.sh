510464029333448c0e23379f45db3e46f0608b57
domenico <domenico@translated.net>
2017-01-12 18:53:03 +0100

    Running Mysql 1 in backgroud and added while sleep loop to attach to the container if needed

--- MateCat-Trusty/MySQL/run.sh
+++ MateCat-Trusty/MySQL/run.sh
@@ -3,4 +3,9 @@
 source /tmp/create_mysql_admin_user.sh
 
 echo "Executing: mysqld_safe --defaults-file=/etc/mysql/my.cnf --user=mysql --plugin-dir=/usr/lib/mysql/plugin"
-mysqld_safe --defaults-file=/etc/mysql/my.cnf --user=mysql --plugin-dir=/usr/lib/mysql/plugin
+mysqld_safe --defaults-file=/etc/mysql/my.cnf --user=mysql --plugin-dir=/usr/lib/mysql/plugin &
+
+while true; do
+#    echo date " => Waiting for an infinite. More or less..."
+    sleep 5
+done
