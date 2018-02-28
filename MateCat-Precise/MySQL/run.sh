0648e0f3ca964c5bdd9d20c4713446737356434d
domenico <domenico@translated.net>
2018-02-28 12:51:14 +0100

    Removed old scripts and files. - Fixed issues with mysql ( restart no more needed )

--- MateCat-Precise/MySQL/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-source /tmp/create_mysql_admin_user.sh
-
-echo "Executing: /usr/bin/mysqld_safe --defaults-file=/etc/mysql/my.cnf --user=mysql --sql-mode=''"
-/usr/bin/mysqld_safe --defaults-file=/etc/mysql/my.cnf --user=mysql --sql-mode=''
--- MateCat-Trusty/MySQL/run.sh
+++ MateCat-Trusty/MySQL/run.sh
@@ -9,10 +9,10 @@ find . -type d | xargs chmod 770
 find . -type f | xargs chmod 660
 popd
 
-source /tmp/create_mysql_admin_user.sh
-
 echo "Executing: mysqld_safe --defaults-file=/etc/mysql/my.cnf --user=mysql --plugin-dir=/usr/lib/mysql/plugin"
 mysqld_safe --defaults-file=/etc/mysql/my.cnf --user=mysql --plugin-dir=/usr/lib/mysql/plugin &
+
+source /tmp/create_mysql_admin_user.sh
 
 while true; do
 #    echo date " => Waiting for an infinite. More or less..."
