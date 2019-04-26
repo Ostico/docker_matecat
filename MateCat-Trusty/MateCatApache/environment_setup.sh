80ed1bf8ddef7c32d8e5376514a1ee05b35d0d6c
domenico <domenico@translated.net>
2019-04-26 14:40:17 +0200

    Upgraded to php 5.6

--- MateCat-Trusty/MateCatApache/environment_setup.sh
+++ MateCat-Trusty/MateCatApache/environment_setup.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 pushd /tmp
-wget http://xdebug.org/files/xdebug-2.5.0.tgz
-tar -xvzf xdebug-2.5.0.tgz
-cd xdebug-2.5.0
+wget https://xdebug.org/files/xdebug-2.5.5.tgz
+tar -xvzf xdebug-2.5.5.tgz
+cd xdebug-2.5.5
 XDEBUG_MODULE_DIR=`phpize | grep 'Zend Module Api No:' | awk '{print $5}'`
 ./configure
 make
-cp modules/xdebug.so /usr/lib/php5/${XDEBUG_MODULE_DIR}
+cp modules/xdebug.so /usr/lib/php/${XDEBUG_MODULE_DIR}
+ln -s /usr/lib/php/${XDEBUG_MODULE_DIR}/xdebug.so /usr/lib/php/5.6/xdebug.so
