a771bca4b115a95ad2e4ccb200efe61c75c38fd9
Ostico Domenico Lupinetti <ostico@gmail.com>
2019-10-21 12:46:01 +0200

    Fixed rdkafka extension

--- MateCat-Bionic/MateCatApache/rdkafka_setup.sh
+++ MateCat-Bionic/MateCatApache/rdkafka_setup.sh
@@ -6,7 +6,7 @@ printf "**** Installing Pecl module rdkafka into /usr/lib/php/${RDKAFKA_MODULE_D
 
 ln -s /usr/lib/php/${RDKAFKA_MODULE_DIR}/rdkafka.so /usr/lib/php/5.6/rdkafka.so
 
-printf "zend_extension=/usr/lib/php/5.6/rdkafka.so\n\n"
-printf "zend_extension=/usr/lib/php/5.6/rdkafka.so" > /etc/php/5.6/mods-available/rdkafka.ini
+printf "extension=/usr/lib/php/5.6/rdkafka.so\n\n"
+printf "extension=/usr/lib/php/5.6/rdkafka.so" > /etc/php/5.6/mods-available/rdkafka.ini
 
 phpenmod rdkafka
