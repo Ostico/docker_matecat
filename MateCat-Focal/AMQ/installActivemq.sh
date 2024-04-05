716b26991d482247ffd4315c5c32c8e656869fc2
domenico <domenico@translated.net>
2024-04-05 18:30:52 +0200

    Improved AMQ

--- MateCat-Focal/AMQ/installActivemq.sh
+++ MateCat-Focal/AMQ/installActivemq.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 printf "Configuring ActiveMq\n"
 
-# skip, already configured
-if [[ -n "${ADM_ACCOUNT}" ]]; then
-    mysqladmin -uroot shutdown
-    return 0
+# Not configured, configure AMQ
+if [ -d "/etc/default/activemq" ]; then
+
+  service activemq create /etc/default/activemq
+
+  chown root:nogroup /etc/default/activemq
+
+  chmod 600 /etc/default/activemq
+
+  sed -i 's/managementContext createConnector="false"/managementContext createConnector="true"/g' /opt/activemq/conf/activemq.xml
+
+  ln -s /etc/init.d/activemq /usr/bin/activemq
+
 fi
 
-/etc/init.d/activemq create /etc/default/activemq
-
-chown root:nogroup /etc/default/activemq
-
-chmod 600 /etc/default/activemq
-
-sed -i 's/managementContext createConnector="false"/managementContext createConnector="true"/g' /opt/activemq/conf/activemq.xml
-
-ln -s /etc/init.d/activemq /usr/bin/activemq
-
-/etc/init.d/activemq start
+service activemq start
 
 RET=1
 while [[ RET -ne 0 ]]; do
-    # echo "=> Waiting an infinite loop ..."
-    sleep 5
+  # echo "=> Waiting an infinite loop ..."
+  sleep 5
 done
--- MateCat-Focal/MateCatFilters/startFilter.sh
+++ MateCat-Focal/MateCatFilters/startFilter.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 printf "Starting Filter\n"
-cd /var/www/MateCat-Filters/ || exit
+cd /var/www/MateCat-Filters/ || exit 1
 
 # shellcheck disable=SC2010
 FILTER_BUILD=$(ls | grep -E "^filters-[0-9\.]+.jar$")
